f3780575aa20e8ea61d77d13aa671fc2d59bb769
Laura M. Castro <lcastro@udc.es>
2021-10-28 10:53:01 +0200

    Initial version: skeleton created.

---
+++ lib/dbus.ex
@@ -0,0 +1,18 @@
+defmodule Dbus do
+  @moduledoc """
+  Documentation for `Dbus`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dbus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/dbus/application.ex
@@ -0,0 +1,20 @@
+defmodule Dbus.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Dbus.Worker.start_link(arg)
+      # {Dbus.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Dbus.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
